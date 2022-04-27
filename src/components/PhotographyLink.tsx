4426c9ee05d3e651744070574162fd4415134bf7
abullard <abullard@leantechniques.com>
2022-04-26 22:45:08 -0500

    multiple images change on div hover

--- src/components/PhotographyLink.tsx
+++ src/components/PhotographyLink.tsx
@@ -1,22 +1,53 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+import HOME_PHOTO from "../assets/photos/home.jpeg";
+import HOME_LIGHTENED_PHOTO from "../assets/photos/home_lightened.jpeg";
+import OFFICE_PHOTO from "../assets/photos/office.jpeg";
+import OFFICE_LIGHTENED_PHOTO from "../assets/photos/office_lightened.jpeg";
 
 const PhotographyLink = () => {
+  const [hovered, setHovered] = useState<Boolean>(false);
+  const [imgOneSource, setImgOneSource] =
+    useState<string>(HOME_LIGHTENED_PHOTO);
+  const [imgTwoSource, setImgTwoSource] = useState<string>(
+    OFFICE_LIGHTENED_PHOTO
+  );
+
+  useEffect(() => {
+    if (hovered) {
+      setImgOneSource(HOME_PHOTO);
+      setImgTwoSource(OFFICE_PHOTO);
+    } else {
+      setImgOneSource(HOME_LIGHTENED_PHOTO);
+      setImgTwoSource(OFFICE_LIGHTENED_PHOTO);
+    }
+  }, [hovered]);
+
   return (
     <Link to="photography">
-      <img
-        height={200}
-        width={350}
-        src={require("../assets/photos/home_lightened.jpeg")}
-        onMouseEnter={(e) =>
-          (e.currentTarget.src = require("../assets/photos/home.jpeg"))
-        }
-        onMouseLeave={(e) =>
-          (e.currentTarget.src = require("../assets/photos/home_lightened.jpeg"))
-        }
-      />
+      <PhotographyImageContainer
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <PhotographyImage1 src={imgOneSource} />
+        <PhotographyImage2 src={imgTwoSource} />
+      </PhotographyImageContainer>
     </Link>
   );
 };
+
+const PhotographyImage1 = (props: any) => {
+  return <img height={200} width={350} src={props.src} />;
+};
+
+const PhotographyImage2 = (props: any) => {
+  return <img height={200} width={150} src={props.src} />;
+};
+
+const PhotographyImageContainer = styled.div`
+  padding: 1rem;
+  display: inline-block;
+`;
 
 export default PhotographyLink;
