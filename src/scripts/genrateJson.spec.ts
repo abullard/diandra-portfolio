612730838028ce501b4c46de03f61523dffdeb45
abullard <abullard@leantechniques.com>
2022-07-15 12:01:29 -0500

    added better prettier config

--- src/scripts/genrateJson.spec.ts
+++ src/scripts/genrateJson.spec.ts
@@ -1,53 +1,42 @@
-import { generateJson } from "./generateJson";
-import { chance } from "../utils/test-utils";
-import { fileSystemService } from "./fileSystemService";
+import { generateJson } from './generateJson';
+import { chance } from '../utils/test-utils';
+import { fileSystemService } from './fileSystemService';
 
-jest.mock("./fileSystemService");
-const mockFileSystemService = fileSystemService as jest.Mocked<
-  typeof fileSystemService
->;
+jest.mock('./fileSystemService');
+const mockFileSystemService = fileSystemService as jest.Mocked<typeof fileSystemService>;
 
-describe("generate_json", () => {
-  let photography_photo_list: string[];
-  let architecture_photo_list: string[];
-  let art_photo_list: string[];
+describe('generate_json', () => {
+    let photography_photo_list: string[];
+    let architecture_photo_list: string[];
+    let art_photo_list: string[];
 
-  beforeEach(() => {
-    photography_photo_list = [chance.word()];
-    architecture_photo_list = [chance.word()];
-    art_photo_list = [chance.word()];
-  });
+    beforeEach(() => {
+        photography_photo_list = [chance.word()];
+        architecture_photo_list = [chance.word()];
+        art_photo_list = [chance.word()];
+    });
 
-  it("should read the photography directory", () => {
-    testWrapper(photography_photo_list);
-    expect(mockFileSystemService.readdirAsync).toHaveBeenCalledWith(
-      1,
-      "../assets/dynamic_photos/photography"
-    );
-  });
+    it('should read the photography directory', () => {
+        testWrapper(photography_photo_list);
+        expect(mockFileSystemService.readdirAsync).toHaveBeenCalledWith(1, '../assets/dynamic_photos/photography');
+    });
 
-  it("should read the architecture directory", async () => {
-    testWrapper(architecture_photo_list);
-    expect(mockFileSystemService.readdirAsync).toHaveBeenNthCalledWith(
-      2,
-      "../assets/dynamic_photos/architecture"
-    );
-  });
+    it('should read the architecture directory', async () => {
+        testWrapper(architecture_photo_list);
+        expect(mockFileSystemService.readdirAsync).toHaveBeenNthCalledWith(2, '../assets/dynamic_photos/architecture');
+    });
 
-  it("should read the art directory", () => {
-    testWrapper(art_photo_list);
-    expect(mockFileSystemService.readdirAsync).toHaveBeenNthCalledWith(
-      3,
-      "../assets/dynamic_photos/art"
-    );
-  });
+    it('should read the art directory', () => {
+        testWrapper(art_photo_list);
+        expect(mockFileSystemService.readdirAsync).toHaveBeenNthCalledWith(3, '../assets/dynamic_photos/art');
+    });
 
-  // it("should read sub-directories, categories, returned by readdir", () => {
-  //   expect(true).toEqual(false);
-  // });
+    // it("should read sub-directories, categories, returned by readdir", () => {
+    //   expect(true).toEqual(false);
+    // });
 });
 
 const testWrapper = async (photoList: string[]) => {
-  await generateJson();
-  mockFileSystemService.readdirAsync.mockResolvedValueOnce(photoList);
+    await generateJson();
+    mockFileSystemService.readdirAsync.mockResolvedValueOnce(photoList);
 };
--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,12 +1,12 @@
-import { render } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ReactElement } from "react";
-import Chance from "chance";
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ReactElement } from 'react';
+import Chance from 'chance';
 
 export const chance = new Chance() as Chance.Chance;
 
 const CustomRender = (component: ReactElement) => {
-  return render(<Router>{component}</Router>);
+    return render(<Router>{component}</Router>);
 };
 
 export { CustomRender as render };
